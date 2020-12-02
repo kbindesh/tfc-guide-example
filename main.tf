033075cdad23368a07e83449b198200499b32ab8
Bindesh Kushwaha <30317164+kbindesh@users.noreply.github.com>
2020-12-02 15:30:15 +0530

    Updated the Main with EC2 instance code

--- main.tf
+++ main.tf
@@ -1,24 +1,53 @@
 provider "aws" {
-  version = "2.33.0"
-
-  region = var.aws_region
+  shared_credentials_file = "/Users/dell/.aws/creds"
+  profile = "default"
+  region  = "ap-south-1"
 }
 
-provider "random" {
-  version = "2.2"
+resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.example.id
+  instance_id = aws_instance.web.id
 }
 
-resource "random_pet" "table_name" {}
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.16.0.0/16"
 
-resource "aws_dynamodb_table" "tfc_example_table" {
-  name = "${var.db_table_name}-${random_pet.table_name.id}"
-
-  read_capacity  = var.db_read_capacity
-  write_capacity = var.db_write_capacity
-  hash_key       = "UUID"
-
-  attribute {
-    name = "UUID"
-    type = "S"
+  tags = {
+    Name = "Test VPC"
   }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "Test Subnet"
+  }
+}
+
+resource "aws_instance" "web" {
+  ami               = "ami-08f63db601b82ff5f"
+  availability_zone = "ap-south-1a"
+  instance_type     = "t2.micro"
+  subnet_id   = aws_subnet.my_subnet.id
+  private_ip = "172.16.10.100"
+  user_data = <<-EOF
+                #! /bin/bash
+                sudo yum install httpd -y
+                sudo systemctl start httpd
+                sudo systemctl enable httpd
+                echo "<h1>Sample Webserver ITC COE Server" | sudo tee  /var/www/html/index.html
+  EOF
+
+  tags = {
+    Name = "TestServer"
+  }
+}
+
+resource "aws_ebs_volume" "example" {
+  availability_zone = "ap-south-1a"
+  size              = 1
 }
